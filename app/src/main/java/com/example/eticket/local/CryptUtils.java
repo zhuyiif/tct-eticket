189e12a7babbd1280f4193619b6f772b770ba567
hanqiankun <hanqiankun@gmail.com>
2018-08-26 15:44:13 +0800

    add crypt util with JNI

---
+++ app/src/main/java/com/example/eticket/local/CryptUtils.java
@@ -0,0 +1,26 @@
+package com.example.eticket.local;
+
+public class CryptUtils {
+
+    public static native byte[] sha1(byte in[], int length);
+
+    public static native byte[] sm3(byte in[], int length);
+
+    public static native byte[] aesEnc(byte in[], int length, byte key[]);
+
+    public static native byte[] aesDec(byte in[], int length, byte key[]);
+
+    public static native byte[] sm4Enc(byte in[], int length, byte key[]);
+
+    public static native byte[] sm4Dec(byte in[], int length, byte key[]);
+
+    public static native byte[] sm2Enc(byte in[], int length);
+
+    public static native byte[] sm2Dec(byte in[], int length);
+
+    public static native byte[] sm2Sign(byte in[], int length);
+
+    public static native int sm2Verify(byte in[], int length, byte sign[], int signLen);
+
+    public static native int genSM2KeyPairs(String path);
+}
